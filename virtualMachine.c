cecdd582f6e88f68d2cb051168386398b01364cd
xaizko <tonytran177013@gmail.com>
2025-06-01 16:04:20 -0400

    Not working execution

--- virtualMachine.c
+++ virtualMachine.c
@@ -48,12 +48,12 @@ Program *exampleprogram(VM *vm) {
 
     i1 = (Instruction *)malloc($i s1);
     i2 = (Instruction *)malloc($i s2);
-    i3 = (Instruction *)malloc($i s2);
+    i3 = (Instruction *)malloc($i map(hlt));
 
-    assert(i1 && i2);
+    assert(i1 && i2 && i3);
     zero($8 i1, s1);
     zero($8 i2, s2);
-    zero($8 i3, s2);
+    zero($8 i3, map(hlt));
 
     i1->o = mov;
     sa1 = (s1-1);
@@ -64,8 +64,8 @@ Program *exampleprogram(VM *vm) {
     p++;
 
     if (a1) {
-	copy($8 p, $8 &a1, sa1);
-	p += sa1;
+        copy($8 p, $8 &a1, sa1);
+        p += sa1;
     }
 
     i2->o = nop;
@@ -75,98 +75,108 @@ Program *exampleprogram(VM *vm) {
     i3->o = hlt;
     copy($8 p, $8 i3, 1);
 
-    vm->b = (s1+sa1+s2);
+    vm->b = (s1+sa1+s2+map(hlt));
     vm->c.r.ip = (Reg)vm->m;
     vm->c.r.sp = (Reg)-1;
 
     free(i1);
     free(i2);
+    free(i3);
 
-
-    return (Program *)&vm->m;
+    return (Program *)vm->m;
 }
 
 int8 map(Opcode o) {
-    int8 n, ret;
+    int8 n;
     IM *p;
-
-    for (n=IMs; p=instrmap, n; n--, p++) {
-	if (p->o == o) {
-	    ret = p->size;
-	    break;
-	}
+    
+    for (n=0, p=instrmap; n < IMs; n++, p++) {
+        if (p->o == o) {
+            return p->size;
+        }
     }
-    return ret;
+    return 0; // Return 0 for unknown opcodes
 }
 
+// Move value into register ax
 void __mov(VM *vm, Opcode opcode, Args a1, Args a2) {
     vm->c.r.ax = (Reg)a1;
-
     return;
 }
 
+// Execute a single instruction based on its opcode
 void execinstr(VM *vm, Instruction *i) {
     Args a1, a2;
     int16 size;
 
-    a1=a2 = 0;
-
+    a1 = a2 = 0;
     size = map(i->o);
 
+    // Extract arguments based on instruction size
     switch (size) {
-	case 1:
-	    break;
-	case 2:
-	    a1 = i->a[0];
-	    break;
-	case 3:
-	    a1 = i->a[0];
-	    a2 = i->a[1];
-	    break;
-	default:
-	    segfault(vm);
-	    break;
+        case 1:
+            break;
+        case 2:
+            a1 = i->a[0];
+            break;
+        case 3:
+            a1 = i->a[0];
+            a2 = i->a[1];
+            break;
+        default:
+            segfault(vm);
+            return;
     }
 
+    // Execute appropriate instruction
     switch (i->o) {
-	case mov:
-	    __mov(vm, i->o, a1, a2);
-	    break;
-	case nop:
-	    break;
-	case hlt:
-	    error(vm, SysHlt);
-	    break;
+        case mov:
+            __mov(vm, i->o, a1, a2);
+            break;
+        case nop:
+            break;
+        case hlt:
+            error(vm, SysHlt);
+            break;
+        default:
+            segfault(vm);
     }
-
-    return;
 }
 
+// Move VM instruction pointer and execute instructions
 void execute(VM *vm) {
     Program *pp;
     int32 brkaddr;
-    Instruction ip; //instruction pointer
+    Instruction *ip; // instruction pointer
     int16 size;
 
-    assert(vm && *vm->m);
-    brkaddr = ((int32)vm->m + vm->b);
-    pp = (Program *)&vm->m;
+    size = 0;
+    assert(vm && vm->m[0]); // Check if VM and memory are initialized
+    printf("memory addr %p\n", vm->m);
+    printf("break line %d\n", vm->b);
+    brkaddr = (int32)(vm->m) + vm->b;
+    pp = vm->m;
 
-    while ((*pp != (Opcode)hlt) && ((int32)pp <= (int32)brkaddr)) {
-
-	printf("pp = %p\n", pp);
-	printf("brk = 0x%x\n", $i brkaddr);
-
-	ip.o = *pp;
-	size = map(ip.o);
-	execinstr(vm, &ip);
-	
-	vm->c.r.ip += size;
-	pp += size;
-    }
-    if ((int32)&pp > brkaddr) {
-	segfault(vm);
-    }
+    do {
+        vm->c.r.ip = (Reg)(int32)pp;
+        ip = (Instruction *)pp;
+        
+        // Check memory bounds before accessing
+        if ((int32)pp >= brkaddr) {
+            segfault(vm);
+        }
+        
+        size = map(ip->o);
+        if (size == 0) {
+            segfault(vm); // Invalid instruction
+        }
+        
+        execinstr(vm, ip);
+        pp += size; // Move to next instruction
+        
+        printf("pp = %p\n", pp);
+        printf("brk = 0x%x\n", brkaddr);
+    } while (*pp != hlt && (int32)pp < brkaddr);
 }
 
 void error(VM* vm, Errorcode e) {
